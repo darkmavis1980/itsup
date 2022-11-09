57f87f21ce731c193d64648418d973efb7daa0d9
Alessio Michelini <amichelini@hubspot.com>
2022-11-09 13:19:30 +0000

    Added linter Using SvelteKit properly Updated configuration

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,12 +1,12 @@
-import sveltePreprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto';
+import sveltePreprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   kit: {
-    files: {
-      routes: 'src/routes',
-    },
+    adapter: adapter(),
   },
   preprocess: sveltePreprocess()
 }
